19b0edc9b446df0fab01c2c7f7bcf490eb130c79
Arbie Rb <arbie2000@gmail.com>
2019-11-15 13:35:21 +0100

    lagt till ul på index filen

--- todo.js
+++ todo.js
@@ -1,14 +1,24 @@
 let list = ['go to gym', 'study'];
+show_listItems();
 
 
+// add items to list
 $('#submit').click(function (){
     let input = $('#the-input');
     inputValue = input.val();
-    list.push()
-
+    list.push(inputValue);
+    $('ul').append('<li>' + inputValue + '</li>');
 })
 
 // Toggle settings-area when button 
 $("#settings").click(function(){
     $("#settings-area").toggle();
   });
+  
+//show list on page
+function show_listItems(){
+  for(let i=0; i<list.length; i++){       
+      let value = list[i];
+        $('ul').append('<li>' + value + '</li>');
+    }
+}
